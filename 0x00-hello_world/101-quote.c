6f56b9ed68296fb84c3e552d9d8f31f8a09fdd95
Robsen Gelana <robinsongelana21@gmail.com>
2023-02-16 06:29:34 -0800

    simple c programm that display a text using putchar function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - display a text with put using printf and puts
+ * 
+ * Return: 1 after code excute
+ */
+
+int main(void)
+{
+	char ch = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	putchar(ch);
+	return (1);
+}
