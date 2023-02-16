915d410b36ee7b57cdbc3d5ebc229c76e8071464
Robsen Gelana <robinsongelana21@gmail.com>
2023-02-16 05:24:09 -0800

    fixing comment mistake

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-/* This main function simply prints a text at the user
+/**
+ * This main function simply prints a text at the user
  * After excuting the code retuns 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
