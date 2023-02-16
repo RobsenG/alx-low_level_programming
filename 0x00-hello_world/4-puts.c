3dfc42e4cf1a1585e70fa5a9aecd297efd174ee6
Robsen Gelana <robinsongelana21@gmail.com>
2023-02-16 05:22:46 -0800

    adding a comment that describ the main fucntion

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/* This main function simply prints a text at the user
+ * After excuting the code retuns 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
