589286dc8618691634cf7f71446b94d1db39adab
Robsen Gelana <robinsongelana21@gmail.com>
2023-02-16 05:33:54 -0800

    simple c programm that print a text to the user using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this function simply displays a text using printf function
+ * Return: 0 after the code excute
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
