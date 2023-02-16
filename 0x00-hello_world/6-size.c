2c2cbe094eb292a65056202493ef04ae0a030621
Robsen Gelana <robinsongelana21@gmail.com>
2023-02-16 06:06:21 -0800

    changing the place holder of the datatypes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,14 +6,11 @@
 
 int main(void)
 {
-	int i;
-	char c;
-	float f;
-	printf("Size of a char: %c bytes(s)\n", sizeof(c));
-	printf("Size of an int: %d bytes(s)\n", sizeof(i));
-	printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a long long int: %lli bytes(s)\n", (unsigned long long)sizeof(i));
-	printf("Size of a float: %f bytes(s)\n", sizeof(f));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int);
+	printf("Size of a long long int: %d  byte(s)\n", sizeof(long long int);
+	printf("Size of a float: %d byte(s)\n", sizeof(f));
 	return (0);
 }
 
