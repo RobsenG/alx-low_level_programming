5b9b12515a91e90bb75b24ae2e86914b7ea90f1e
Robsen Gelana <robinsongelana21@gmail.com>
2023-02-17 05:35:43 -0800

    c programm that print base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This function print base 10 numbers
+ *
+ * Return: 0 Every time the run(Excute)
+ */
+int main(void)
+{
+	int i;
+	for (i=0; i<10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return(0);
+
+}
