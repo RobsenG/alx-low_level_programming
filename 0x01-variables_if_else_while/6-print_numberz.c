fad268636899ca9facd88611cb229603e8d89da6
Robsen Gelana <robinsongelana21@gmail.com>
2023-02-17 10:08:13 -0800

    c programm that print base 10 digit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This function that print all single digit number with base 10
+ *
+ * Return: 0 Every time the code run(Excute)
+ */
+int main (void)
+{
+	int i;
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return(0);
+
+}
